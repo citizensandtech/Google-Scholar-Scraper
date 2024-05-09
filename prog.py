44530c29a4f7250329419592e5297c6ba47aa1c8
J. Nathan Matias <natematias@gmail.com>
2024-05-09 11:23:30 -0400

    added a query date column and changed the filname to include the timestamp and not overwhite the excel file.

--- prog.py
+++ prog.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import datetime
 
 def scrape_scholar_articles(query, num_pages):
     articles = []
     page = 0
+
+    query_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
     while page < num_pages:
         url = f"https://scholar.google.com/scholar?start={page*10}&q={query}&hl=en&as_sdt=0,5"
         response = requests.get(url)
@@ -17,7 +21,7 @@ def scrape_scholar_articles(query, num_pages):
             title = result.find("h3", class_="gs_rt").text
             authors = result.find("div", class_="gs_a").text
             link = result.find("a")["href"]
-            articles.append({"Title": title, "Authors": authors, "Link": link})
+            articles.append({"Query Date": query_date,"Title": title, "Authors": authors, "Link": link})
 
         page += 1
 
@@ -39,13 +43,17 @@ def scrape_articles():
     articles = scrape_scholar_articles(query, num_pages)
 
     folder_path = entry_folder.get()
+  
+    unix_timestamp = int(datetime.datetime.now().timestamp())
+
+    filename_string = str(unix_timestamp) + "-scholar_articles.xlsx"
     if folder_path:
-        filename = f"{folder_path}/scholar_articles.xlsx"
+        filename = f"{folder_path}/" + filename_string
     else:
-        filename = "scholar_articles.xlsx"
+        filename = filename_string
 
     save_to_excel(articles, filename)
-    label_status.config(text="Extraction complete. Data saved to scholar_articles.xlsx.")
+    label_status.config(text="Extraction complete. Data saved to" + filename_string)
 
 # Create the main window
 window = tk.Tk()
